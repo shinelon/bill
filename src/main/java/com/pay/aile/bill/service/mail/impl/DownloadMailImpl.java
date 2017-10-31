34dc051f8f9036464c59221310df77360f5a9178
shinelon <syq_sunshine@163.com>
2017-10-31 10:01:32 +0800

    modify login

--- src/main/java/com/pay/aile/bill/service/mail/impl/DownloadMailImpl.java
+++ src/main/java/com/pay/aile/bill/service/mail/impl/DownloadMailImpl.java
@@ -1,14 +1,10 @@
 package com.pay.aile.bill.service.mail.impl;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.pay.aile.bill.enums.MailType;
 import com.pay.aile.bill.service.mail.DownloadMail;
 import com.pay.aile.bill.service.mail.MailOperation;
 import com.pay.aile.bill.utils.MailTypeUtil;
@@ -27,17 +23,9 @@ public class DownloadMailImpl implements DownloadMail {
 
     private static final Logger logger = LoggerFactory.getLogger(DownloadMailImpl.class);
 
-    private static Map<MailType, Class<? extends MailOperation>> mailOperationMap = new HashMap<>(32);
-    static {
-        mailOperationMap.put(MailType.Mail_126, Mail126OperationImpl.class);
-    }
-
     @Override
     public void execute(final String mailAddr, final String password) throws Exception {
         String[] mailParms = StringUtils.split(mailAddr, "@");
-        // Class<? extends MailOperation> clzz =
-        // mailOperationMap.get(MailTypeUtil.getMailType(mailParms[1]));
-        // MailOperation mailOperation = clzz.newInstance();
         MailOperation mailOperation = SpringContextUtil.getBean(MailTypeUtil.getMailType(mailParms[1]).getClzz());
         mailOperation.downloadMail(mailAddr, password);
 
--- src/main/java/com/pay/aile/bill/service/mail/impl/Mail126OperationImpl.java
+++ src/main/java/com/pay/aile/bill/service/mail/impl/Mail126OperationImpl.java
@@ -1,12 +1,8 @@
 package com.pay.aile.bill.service.mail.impl;
 
-import java.util.Properties;
-
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.NoSuchProviderException;
-import javax.mail.Session;
 import javax.mail.Store;
 
 import org.slf4j.Logger;
@@ -16,6 +12,7 @@ import org.springframework.stereotype.Service;
 import com.pay.aile.bill.exception.MailBillException;
 import com.pay.aile.bill.service.mail.MailOperation;
 import com.pay.aile.bill.utils.DownloadUtil;
+import com.pay.aile.bill.utils.MailLoginUtil;
 import com.pay.aile.bill.utils.MailReleaseUtil;
 import com.pay.aile.bill.utils.MailSearchUtil;
 
@@ -33,7 +30,7 @@ public class Mail126OperationImpl implements MailOperation {
 
     @Override
     public void downloadMail(String mailAddr, String password) throws Exception {
-        Store store = login(mailAddr, password);
+        Store store = MailLoginUtil.login("pop.126.com", mailAddr, password);
         Folder defaultFolder = null;
         Folder[] folder_arr = null;
         try {
@@ -65,41 +62,6 @@ public class Mail126OperationImpl implements MailOperation {
      */
     private String getKeywords() {
         return "中信银行";
-    }
-
-    /***
-     * 自定义登录页面 注意使用后是否store
-     *
-     * @param mailAddrWithoutSuffix
-     * @param password
-     * @return
-     * @throws MailBillException
-     */
-    private Store login(final String mailAddrWithoutSuffix, final String password) throws MailBillException {
-        logger.info("begin landing user mail:{}", mailAddrWithoutSuffix);
-        Properties props = new Properties();
-        props.setProperty("mail.store.protocol", "pop3"); // 协议
-        props.setProperty("mail.pop3.port", "110"); // 端口
-        props.setProperty("mail.pop3.host", "pop.126.com");// 服务器地址
-        // 创建Session实例对象
-        Session session = Session.getInstance(props);
-        // 创建pop3协议的Store对象
-        try {
-            Store store = session.getStore("pop3");
-            store.connect(mailAddrWithoutSuffix, password);
-            boolean connected = store.isConnected();
-            logger.info("mail:{} \tis connected:{}", mailAddrWithoutSuffix, connected);
-            if (!connected) {
-                throw new MailBillException("");
-            }
-            return store;
-        } catch (NoSuchProviderException e) {
-            e.printStackTrace();
-            throw new MailBillException("");
-        } catch (MessagingException e) {
-            e.printStackTrace();
-            throw new MailBillException("");
-        }
     }
 
 }
