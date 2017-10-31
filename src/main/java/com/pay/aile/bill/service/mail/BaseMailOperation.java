eb548b8e963d9b3e98ee127dde94813339926981
shinelon <syq_sunshine@163.com>
2017-10-31 10:33:12 +0800

    modify BaseMailOperation

---
+++ src/main/java/com/pay/aile/bill/service/mail/BaseMailOperation.java
@@ -0,0 +1,61 @@
+package com.pay.aile.bill.service.mail;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Store;
+
+import com.pay.aile.bill.exception.MailBillException;
+import com.pay.aile.bill.utils.DownloadUtil;
+import com.pay.aile.bill.utils.MailLoginUtil;
+import com.pay.aile.bill.utils.MailReleaseUtil;
+import com.pay.aile.bill.utils.MailSearchUtil;
+
+/***
+ * MailOperation.java
+ *
+ * @author shinelon
+ *
+ * @date 2017年10月30日
+ *
+ */
+public abstract class BaseMailOperation {
+    /***
+     * 下载邮件
+     *
+     * 注意126、163、和qq邮箱使用POP3授权码
+     *
+     * @param mailAddr
+     * @param password
+     * @throws Exception
+     */
+    public void downloadMail(String mailAddr, String password) throws Exception {
+        Store store = MailLoginUtil.login(getPop3ServerAddr(), mailAddr, password);
+        Folder defaultFolder = null;
+        Folder[] folder_arr = null;
+        try {
+            defaultFolder = store.getDefaultFolder();
+            folder_arr = defaultFolder.list();
+            for (Folder tempFolder : folder_arr) {
+                Folder folder = store.getFolder(tempFolder.getName());
+                folder.open(Folder.READ_ONLY);
+                Message[] messages = MailSearchUtil.search(getKeywords(), folder);
+                for (int i = 0; i < messages.length; i++) {
+                    DownloadUtil.saveFile(messages[i], i);
+                }
+                folder.close(true);
+            }
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (MailBillException e) {
+            e.printStackTrace();
+        } finally {
+            MailReleaseUtil.releaseFolderAndStore(defaultFolder, store);
+        }
+    }
+
+    public abstract String getKeywords();
+
+    public abstract String getPop3ServerAddr();
+}
--- src/main/java/com/pay/aile/bill/service/mail/impl/Mail126OperationImpl.java
+++ src/main/java/com/pay/aile/bill/service/mail/impl/Mail126OperationImpl.java
@@ -1,20 +1,10 @@
 package com.pay.aile.bill.service.mail.impl;
 
-import javax.mail.Folder;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Store;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.pay.aile.bill.exception.MailBillException;
-import com.pay.aile.bill.service.mail.MailOperation;
-import com.pay.aile.bill.utils.DownloadUtil;
-import com.pay.aile.bill.utils.MailLoginUtil;
-import com.pay.aile.bill.utils.MailReleaseUtil;
-import com.pay.aile.bill.utils.MailSearchUtil;
+import com.pay.aile.bill.service.mail.BaseMailOperation;
 
 /***
  * MailOperationImpl.java
@@ -25,43 +15,22 @@ import com.pay.aile.bill.utils.MailSearchUtil;
  *
  */
 @Service
-public class Mail126OperationImpl implements MailOperation {
+public class Mail126OperationImpl extends BaseMailOperation {
     private static final Logger logger = LoggerFactory.getLogger(Mail126OperationImpl.class);
-
-    @Override
-    public void downloadMail(String mailAddr, String password) throws Exception {
-        Store store = MailLoginUtil.login("pop.126.com", mailAddr, password);
-        Folder defaultFolder = null;
-        Folder[] folder_arr = null;
-        try {
-            defaultFolder = store.getDefaultFolder();
-            folder_arr = defaultFolder.list();
-            for (Folder tempFolder : folder_arr) {
-                Folder folder = store.getFolder(tempFolder.getName());
-                folder.open(Folder.READ_ONLY);
-                Message[] messages = MailSearchUtil.search(getKeywords(), folder);
-                for (int i = 0; i < messages.length; i++) {
-                    DownloadUtil.saveFile(messages[i], i);
-                }
-                folder.close(true);
-            }
-
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        } catch (MailBillException e) {
-            e.printStackTrace();
-        } finally {
-            MailReleaseUtil.releaseFolderAndStore(defaultFolder, store);
-        }
-    }
 
     /***
      * 获取搜索邮件关键字
      *
      * @return
      */
-    private String getKeywords() {
+    @Override
+    public String getKeywords() {
         return "中信银行";
+    }
+
+    @Override
+    public String getPop3ServerAddr() {
+        return "pop.126.com";
     }
 
 }
