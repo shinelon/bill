9ddab58b63625c45d215875067bf9dede6fa403b
charlie <1151832099@qq.com>
2017-10-31 10:47:27 +0800

    将count从AtomicInteger改为int

--- src/main/java/com/pay/aile/bill/service/mail/analyze/banktemplate/BaseBankTemplate.java
+++ src/main/java/com/pay/aile/bill/service/mail/analyze/banktemplate/BaseBankTemplate.java
@@ -1,7 +1,6 @@
 package com.pay.aile.bill.service.mail.analyze.banktemplate;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import com.pay.aile.bill.service.mail.analyze.BankMailAnalyzerTemplate;
 
@@ -16,18 +15,18 @@ public abstract class BaseBankTemplate
     /**
      * 统计每一种模板的调用次数
      */
-    private volatile AtomicInteger count = new AtomicInteger(0);
+    private volatile int count;
 
     @Override
     public int compareTo(BaseBankTemplate o) {
         if (o == null)
             return 1;
-        return count.get() > o.count.get() ? 1 : -1;
+        return count > o.count ? 1 : -1;
     }
 
     @Override
     public void analyze(List<String> content) {
-        count.incrementAndGet();
+        count++;
         analyzeInternal(content);
     }
 
