5759d70e8afccf6e515df6d573a9743d5b3f488d
jinjing <jinjing@windows10.microdone.cn>
2017-11-01 15:43:16 +0800

    增加hotmail(outlook)邮箱下载附件，增加mongodb存储附件

--- src/main/java/com/pay/aile/bill/enums/MailType.java
+++ src/main/java/com/pay/aile/bill/enums/MailType.java
@@ -5,6 +5,7 @@ import com.pay.aile.bill.service.mail.download.impl.Mail126OperationImpl;
 import com.pay.aile.bill.service.mail.download.impl.Mail139OperationImpl;
 import com.pay.aile.bill.service.mail.download.impl.Mail163OperationImpl;
 import com.pay.aile.bill.service.mail.download.impl.MailAliyunOperationImpl;
+import com.pay.aile.bill.service.mail.download.impl.MailHotOperationImpl;
 import com.pay.aile.bill.service.mail.download.impl.MailQQOperationImpl;
 import com.pay.aile.bill.service.mail.download.impl.MailSinaOperationImpl;
 
@@ -18,33 +19,34 @@ import com.pay.aile.bill.service.mail.download.impl.MailSinaOperationImpl;
  */
 public enum MailType {
 
-    MAIL_126("126.com", Mail126OperationImpl.class), MAIL_139("139.com", Mail139OperationImpl.class), MAIL_163(
-            "163.com", Mail163OperationImpl.class), MAIL_ALIYUN("aliyun.com", MailAliyunOperationImpl.class), MAIL_QQ(
-                    "qq.com", MailQQOperationImpl.class), MAIL_SINA("sina.com", MailSinaOperationImpl.class);
+	MAIL_126("126.com", Mail126OperationImpl.class), MAIL_139("139.com", Mail139OperationImpl.class), MAIL_163(
+			"163.com", Mail163OperationImpl.class), MAIL_ALIYUN("aliyun.com", MailAliyunOperationImpl.class), MAIL_HOT(
+					"outlook.com", MailHotOperationImpl.class), MAIL_QQ("qq.com",
+							MailQQOperationImpl.class), MAIL_SINA("sina.com", MailSinaOperationImpl.class);
 
-    public static MailType getMailType(String mailAddrSuffix) {
-        for (MailType tmpMailType : MailType.values()) {
-            if (tmpMailType.getKey().equals(mailAddrSuffix)) {
-                return tmpMailType;
-            }
-        }
-        return null;
-    }
+	public static MailType getMailType(String mailAddrSuffix) {
+		for (MailType tmpMailType : MailType.values()) {
+			if (tmpMailType.getKey().equals(mailAddrSuffix)) {
+				return tmpMailType;
+			}
+		}
+		return null;
+	}
 
-    private Class<? extends BaseMailOperation> classes;
+	private Class<? extends BaseMailOperation> classes;
 
-    private String key;
+	private String key;
 
-    MailType(String key, Class<? extends BaseMailOperation> classes) {
-        this.key = key;
-        this.classes = classes;
-    }
+	MailType(String key, Class<? extends BaseMailOperation> classes) {
+		this.key = key;
+		this.classes = classes;
+	}
 
-    public Class<? extends BaseMailOperation> getClzz() {
-        return classes;
-    }
+	public Class<? extends BaseMailOperation> getClzz() {
+		return classes;
+	}
 
-    public String getKey() {
-        return key;
-    }
+	public String getKey() {
+		return key;
+	}
 }
---
+++ src/test/java/com/pay/aile/bill/mongo/MongoTest.java
@@ -0,0 +1,31 @@
+package com.pay.aile.bill.mongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.pay.aile.bill.BillApplication;
+import com.pay.aile.bill.exception.MailBillException;
+import com.pay.aile.bill.utils.MongoDownloadUtil;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BillApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+public class MongoTest {
+
+	@Autowired
+	private MongoDownloadUtil downloadUtil;
+
+	@Test
+	public void downFile() {
+		try {
+			String s = downloadUtil.getFile("Fw:交通银行信用卡电子账单");
+			System.out.println(s);
+		} catch (MailBillException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
