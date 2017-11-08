e32f49d5b3504de66daae5a2425e8f1e2fc4ec4b
jibing <619623172@qq.com>
2017-11-08 18:17:39 +0800

    添加浦发银行信用卡模板

---
+++ src/main/java/com/pay/aile/bill/service/mail/analyze/banktemplate/spdb/AbstractSPDBTemplate.java
@@ -0,0 +1,13 @@
+package com.pay.aile.bill.service.mail.analyze.banktemplate.spdb;
+
+import com.pay.aile.bill.service.mail.analyze.banktemplate.BaseBankTemplate;
+
+/**
+ *
+ * @author ji
+ * @description 浦发银行
+ *
+ */
+public class AbstractSPDBTemplate extends BaseBankTemplate {
+
+}
