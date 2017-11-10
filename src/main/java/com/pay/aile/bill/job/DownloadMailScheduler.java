022014ef43797ba8315bf9cd360c578b25107b6a
shinelon <syq_sunshine@163.com>
2017-11-10 15:04:12 +0800

    初始化任务列表

--- src/main/java/com/pay/aile/bill/job/DownloadMailScheduler.java
+++ src/main/java/com/pay/aile/bill/job/DownloadMailScheduler.java
@@ -25,82 +25,82 @@ import com.pay.aile.bill.service.mail.download.DownloadMail;
  */
 @Service
 public class DownloadMailScheduler {
-	private static boolean flagJobLoop = true;
-	private static final Logger logger = LoggerFactory.getLogger(DownloadMailScheduler.class);
-	// 当邮箱扫描时间间隔大约loopIntervalSeconds 重新下载邮箱
-	private static final long loopIntervalSeconds = 100L;
+    private static boolean flagJobLoop = true;
+    private static final Logger logger = LoggerFactory.getLogger(DownloadMailScheduler.class);
+    // 当邮箱扫描时间间隔大约loopIntervalSeconds 重新下载邮箱
+    private static final long loopIntervalSeconds = 180L;
 
-	// 当循环线程发现没有邮件需要下载的时候，等待waitSeconds
-	private static final long waitSeconds = 100L;
-	@Autowired
-	private CreditEmailService creditEmailService;
-	@Autowired
-	private DownloadMail downloadMail;
+    // 当循环线程发现没有邮件需要下载的时候，等待waitSeconds
+    private static final long waitSeconds = 10L;
+    @Autowired
+    private CreditEmailService creditEmailService;
+    @Autowired
+    private DownloadMail downloadMail;
 
-	@Autowired
-	private RedisJobHandle redisJobHandle;
+    @Autowired
+    private RedisJobHandle redisJobHandle;
 
-	// @Autowired
-	@Resource(name = "mailTaskExecutor")
-	private ThreadPoolTaskExecutor taskExecutor;
+    // @Autowired
+    @Resource(name = "mailTaskExecutor")
+    private ThreadPoolTaskExecutor taskExecutor;
 
-	public void downLoadMail() {
-		long startTime = System.currentTimeMillis();
-		List<CreditEmail> list = creditEmailService.getCreditEmails();
-		logger.debug("DownloadMailScheduler list:{}", list);
-		CountDownLatch doneSignal = new CountDownLatch(list.size());
-		for (CreditEmail creditEmail : list) {
-			taskExecutor.execute(() -> {
-				try {
-					downloadMail.execute(creditEmail);
-				} catch (Exception e) {
-					logger.warn("download exception:{}", creditEmail);
-				} finally {
-					doneSignal.countDown();
-				}
-			});
-		}
-		try {
-			doneSignal.await();
-			long endTime = System.currentTimeMillis();
-			logger.debug("DownloadMailScheduler done:{}ms", endTime - startTime);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+    public void downLoadMail() {
+        long startTime = System.currentTimeMillis();
+        List<CreditEmail> list = creditEmailService.getCreditEmails();
+        logger.debug("DownloadMailScheduler list:{}", list);
+        CountDownLatch doneSignal = new CountDownLatch(list.size());
+        for (CreditEmail creditEmail : list) {
+            taskExecutor.execute(() -> {
+                try {
+                    downloadMail.execute(creditEmail);
+                } catch (Exception e) {
+                    logger.warn("download exception:{}", creditEmail);
+                } finally {
+                    doneSignal.countDown();
+                }
+            });
+        }
+        try {
+            doneSignal.await();
+            long endTime = System.currentTimeMillis();
+            logger.debug("DownloadMailScheduler done:{}ms", endTime - startTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	public void downLoadMailLoop() {
-		while (flagJobLoop) {
-			CreditEmail creditEmail = redisJobHandle.getJob();
-			if (creditEmail.getId() == null) {
-				// 如果获取不到creditEmail 则任务已经删除
-				continue;
-			}
+    public void downLoadMailLoop() {
+        while (flagJobLoop) {
+            CreditEmail creditEmail = redisJobHandle.getJob();
+            if (creditEmail.getId() == null) {
+                // 如果获取不到creditEmail 则任务已经删除
+                continue;
+            }
 
-			if (System.currentTimeMillis() - creditEmail.getLastJobTimestamp() < loopIntervalSeconds * 1000) {
-				redisJobHandle.doneJob(creditEmail);
-				try {
-					Thread.sleep(waitSeconds * 1000);
-					logger.debug("==job id :{} is done waitSeconds:{} ", creditEmail.getId(), waitSeconds);
-				} catch (InterruptedException e) {
-				}
-				continue;
-			}
-			taskExecutor.execute(() -> {
-				try {
-					logger.debug("id:{}download email:{}", creditEmail.getId(), creditEmail.getEmail());
-					downloadMail.execute(creditEmail, redisJobHandle);
-				} catch (Exception e) {
-					logger.warn("download exception:{}", creditEmail);
-				}
-			});
+            if (System.currentTimeMillis() - creditEmail.getLastJobTimestamp() < loopIntervalSeconds * 1000) {
+                redisJobHandle.doneJob(creditEmail);
+                try {
+                    Thread.sleep(waitSeconds * 1000);
+                    logger.debug("==job id :{} is done waitSeconds:{} ", creditEmail.getId(), waitSeconds);
+                } catch (InterruptedException e) {
+                }
+                continue;
+            }
+            taskExecutor.execute(() -> {
+                try {
+                    logger.debug("id:{}download email:{}", creditEmail.getId(), creditEmail.getEmail());
+                    downloadMail.execute(creditEmail, redisJobHandle);
+                } catch (Exception e) {
+                    logger.warn("download exception:{}", creditEmail);
+                }
+            });
 
-		}
+        }
 
-	}
+    }
 
-	public void offJobLoop() {
-		flagJobLoop = false;
-	}
+    public void offJobLoop() {
+        flagJobLoop = false;
+    }
 }
