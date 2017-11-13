3374be46f8ce736d93ed06b54eaaf97aa4024cf6
shinelon <syq_sunshine@163.com>
2017-11-13 16:26:49 +0800

    modify sql

--- bill_shinelon.sql
+++ bill_shinelon.sql
@@ -33,15 +33,15 @@ CREATE TABLE `credit_account_type` (
 CREATE TABLE `credit_bank` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '银行名称',
-  `for_short` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '简称',
   `ext_keyword` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '扩展主题关键字',
   `online` tinyint(4) DEFAULT NULL COMMENT '是否支持网银',
   `email` tinyint(4) DEFAULT NULL COMMENT '是否支持邮件',
   `status` tinyint(4) DEFAULT NULL COMMENT '有效标志1有效0无效',
   `update_date` datetime DEFAULT NULL COMMENT '修改时间',
   `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `code` varchar(45) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '编码',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='银行';
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='银行';
 
 /*Table structure for table `credit_bill` */
 
@@ -124,27 +124,36 @@ CREATE TABLE `credit_file` (
 /*Table structure for table `credit_template` */
 
 CREATE TABLE `credit_template` (
-  `id` bigint(50) NOT NULL AUTO_INCREMENT,
-  `due_date` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '到期还款日',
-  `current_amount` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '本期应还款额',
-  `credits` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '信用额度',
-  `cash` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '取现额度',
-  `last_amount` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
-  `repayment` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '还款/退货/费用返还',
-  `consumption` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '消费/取现/其他费用',
-  `transaction_date` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '交易日期',
-  `billing_date` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '记账日志',
-  `transaction_description` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '交易说明',
-  `transaction_amount` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '交易币种/金额',
-  `accountable_amount` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '入账币种/金额',
-  `prepaid_cash_amount` decimal(10,2) DEFAULT NULL COMMENT '预借现金额度',
-  `details` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '账单明细',
-  `cardtype_id` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '卡类型',
-  `status` tinyint(4) DEFAULT NULL COMMENT '有效标志1有效0无效',
-  `update_date` datetime DEFAULT NULL COMMENT '修改时间',
-  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `due_date` varchar(200) DEFAULT NULL COMMENT '到期还款日',
+  `current_amount` varchar(200) DEFAULT NULL COMMENT '本期应还款额',
+  `credits` varchar(200) DEFAULT NULL COMMENT '信用额度',
+  `cash` varchar(200) DEFAULT NULL COMMENT '取现额度',
+  `last_amount` varchar(200) DEFAULT NULL,
+  `repayment` varchar(200) DEFAULT NULL COMMENT '还款/退货/费用返还',
+  `consumption` varchar(200) DEFAULT NULL COMMENT '消费/取现/其他费用',
+  `transaction_date` varchar(200) DEFAULT NULL COMMENT '交易日期',
+  `billing_date` varchar(200) DEFAULT NULL COMMENT '记账日志',
+  `transaction description` varchar(200) DEFAULT NULL COMMENT '交易说明',
+  `transaction_amount` varchar(200) DEFAULT NULL COMMENT '交易币种/金额',
+  `accountable_amount` varchar(200) DEFAULT NULL COMMENT '入账币种/金额',
+  `prepaid_cash_amount` varchar(200) DEFAULT NULL COMMENT '预借现金额度',
+  `details` varchar(200) DEFAULT NULL COMMENT '账单明细',
+  `cardtype_id` varchar(200) DEFAULT NULL COMMENT '卡类型',
+  `create_date` datetime DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  `transaction_description` varchar(45) DEFAULT NULL,
+  `cycle` varchar(100) DEFAULT NULL COMMENT '账单周期',
+  `bill_day` varchar(100) DEFAULT NULL,
+  `transaction_currency` varchar(20) DEFAULT NULL COMMENT '交易币种',
+  `cardholder` varchar(45) DEFAULT NULL COMMENT '持卡人',
+  `credit_templatecol` varchar(45) DEFAULT NULL,
+  `year_month` varchar(45) DEFAULT NULL COMMENT '年月',
+  `minimum` varchar(45) DEFAULT NULL,
+  `card_numbers` varchar(45) DEFAULT NULL COMMENT '解析卡号后四位',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='邮件解析模板';
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='邮件解析模板';
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
